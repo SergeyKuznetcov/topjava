df31c1f9be811dc1a51ccb01a9a111b688bc4117
Sergey <kuznecosa96@mail.ru>
2022-11-08 15:48:46 +0300

    HW5 main task

--- src/main/java/ru/javawebinar/topjava/Profiles.java
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -8,7 +8,11 @@ public class Profiles {
             JPA = "jpa",
             DATAJPA = "datajpa";
 
-    public static final String REPOSITORY_IMPLEMENTATION = DATAJPA;
+    public static final String REPOSITORY_IMPLEMENTATION = JDBC;
+
+    public static String getActiveRepositoryImpl(){
+        return REPOSITORY_IMPLEMENTATION;
+    }
 
     public static final String
             POSTGRES_DB = "postgres",
