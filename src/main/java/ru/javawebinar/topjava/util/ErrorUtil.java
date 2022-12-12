855162328311ccd4e060253e7db99bd7015016be
Sergey <kuznecosa96@mail.ru>
2022-12-12 22:43:44 +0300

    HW9 meal binding update validation

---
+++ src/main/java/ru/javawebinar/topjava/util/ErrorUtil.java
@@ -0,0 +1,20 @@
+package ru.javawebinar.topjava.util;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+
+import java.util.stream.Collectors;
+
+public class ErrorUtil {
+    public static ResponseEntity<String> checkFieldErrorsAndDo(BindingResult result, Runnable runnable){
+        if (result.hasErrors()) {
+            String errorFieldsMsg = result.getFieldErrors().stream()
+                    .map(fe -> String.format("[%s] %s", fe.getField(), fe.getDefaultMessage()))
+                    .collect(Collectors.joining("<br>"));
+            return ResponseEntity.unprocessableEntity().body(errorFieldsMsg);
+        }
+        runnable.run();
+        return ResponseEntity.ok().build();
+    }
+
+}
