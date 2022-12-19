9a565a55159fa452f9593b180014dc59ee222b3f
Sergey <kuznecosa96@mail.ru>
2022-12-19 12:45:37 +0300

    HW10 main task

--- src/main/java/ru/javawebinar/topjava/web/user/ProfileUIController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/ProfileUIController.java
@@ -1,16 +1,23 @@
 package ru.javawebinar.topjava.web.user;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.support.SessionStatus;
 import ru.javawebinar.topjava.to.UserTo;
+import ru.javawebinar.topjava.util.exception.ErrorInfo;
 import ru.javawebinar.topjava.web.SecurityUtil;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+
+import static ru.javawebinar.topjava.util.exception.ErrorType.EXCEPTION_DUPLICATE_EMAIL;
 
 @Controller
 @RequestMapping("/profile")
@@ -50,5 +57,10 @@ public class ProfileUIController extends AbstractUserController {
             status.setComplete();
             return "redirect:/login?message=app.registered&username=" + userTo.getEmail();
         }
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorInfo> duplicateEmailException(HttpServletRequest req, DataIntegrityViolationException e) {
+        return ResponseEntity.unprocessableEntity().body(new ErrorInfo(req.getRequestURL(), EXCEPTION_DUPLICATE_EMAIL, "[email] User with this email already exists"));
     }
 }
--- src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -81,6 +81,16 @@ class MealRestControllerTest extends AbstractControllerTest {
     }
 
     @Test
+    void updateWithIllegalData() throws Exception {
+        Meal updated = getUpdated();
+        updated.setDescription("");
+        perform(MockMvcRequestBuilders.put(REST_URL + MEAL1_ID).contentType(MediaType.APPLICATION_JSON)
+                .with(userHttpBasic(user))
+                .content(JsonUtil.writeValue(updated)))
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
     void createWithLocation() throws Exception {
         Meal newMeal = getNew();
         ResultActions action = perform(MockMvcRequestBuilders.post(REST_URL)
@@ -94,6 +104,17 @@ class MealRestControllerTest extends AbstractControllerTest {
         newMeal.setId(newId);
         MEAL_MATCHER.assertMatch(created, newMeal);
         MEAL_MATCHER.assertMatch(mealService.get(newId, USER_ID), newMeal);
+    }
+
+    @Test
+    void createWithIllegalData() throws Exception {
+        Meal newMeal = getNew();
+        newMeal.setDescription("");
+        perform(MockMvcRequestBuilders.post(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(userHttpBasic(user))
+                .content(JsonUtil.writeValue(newMeal)))
+                .andExpect(status().isUnprocessableEntity());
     }
 
     @Test
